f7c21468478762f39cca9488bbef83118895db92
Sufian Idris <sufian.idris@gmail.com>
2023-07-25 15:18:33 +0800

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Sufian Bin Idris on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
